d081083b19071fdba99e50e505fd29d7da14c7e8
Abhijit Muley <abhijitrmly@gmail.com>
2020-06-23 00:20:11 +0530

    fix api url for bill fetch receipt

--- src/services/bills-fetch-receipt/bills-fetch-receipt.service.js
+++ src/services/bills-fetch-receipt/bills-fetch-receipt.service.js
@@ -10,10 +10,10 @@ module.exports = function (app) {
   };
 
   // Initialize our service with any options it requires
-  app.use('/bills/fetchReceipt', new BillsFetchReceipt(options, app));
+  app.use('/bill/fetchReceipt', new BillsFetchReceipt(options, app));
 
   // Get our initialized service so that we can register hooks
-  const service = app.service('bills/fetchReceipt');
+  const service = app.service('bill/fetchReceipt');
 
   service.hooks(hooks);
 };
