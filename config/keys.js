61e12de63a0280e78a72c50d5f637985f7a69490
Abhijit Muley <abhijitrmly@gmail.com>
2020-06-23 23:16:15 +0530

    add mlab credentials for database

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://abhijitrmly:biller2206@ds033484.mlab.com:33484/biller2206'
+};
--- src/mongoose.js
+++ src/mongoose.js
@@ -1,15 +1,27 @@
 const mongoose = require('mongoose');
-const logger = require('./logger');
+// const logger = require('./logger');
+// DB Config
+const db = require('../config/keys').mongoURI;
 
 module.exports = function (app) {
-  mongoose.connect(
-    app.get('mongodb'),
-    { useCreateIndex: true, useNewUrlParser: true }
-  ).catch(err => {
-    logger.error(err);
-    process.exit(1);
-  });
-  
+
+  // Connect to MongoDB
+  mongoose
+    .connect(
+      db,
+      { useNewUrlParser: true }
+    )
+    .then(() => console.log('MongoDB successfully connected'))
+    .catch(err => console.log(err));
+
+  // mongoose.connect(
+  //   app.get('mongodb'),
+  //   { useCreateIndex: true, useNewUrlParser: true }
+  // ).catch(err => {
+  //   logger.error(err);
+  //   process.exit(1);
+  // });
+
   mongoose.Promise = global.Promise;
 
   app.set('mongooseClient', mongoose);
